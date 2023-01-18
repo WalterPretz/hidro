2c9d8a84a7f346d553c55063638daba6101422c9
Walter Pretzantzín <pretz23jer@gmail.com>
2023-01-17 19:29:00 -0600

    Modificando archivos para estilos

--- resource/js/script.js
+++ resource/js/script.js
@@ -1,12 +1,4 @@
 //iniciar aos
 AOS.init();
 
-/*traer dado de bombas*/
-
-$(document).ready(function(){ 
-	$.post('traerData').done(function(respuesta){
-		console.log(respuesta);
-		data = $.parseJSON(respuesta);
-		$('#resultadoBomba').html(data.resultado);
-	})
-});
+console.log('pasa por este archivo');
